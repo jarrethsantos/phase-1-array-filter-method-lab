6d3eee1b5ab86579e8b0b34f77b7edd691218029
Jarreth Santos <santos.jarreth@gmail.com>
2024-03-05 16:05:03 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution here
+function findMatching (arrayOfDrivers, string) {
+    const matchArray = [];
+    
+    for (const driver of arrayOfDrivers) {
+        if (driver.toLowerCase() === string.toLowerCase()) {
+            matchArray.push(driver);
+        }
+    } 
+
+    return matchArray;
+    
+} 
+
+
+function fuzzyMatch (arrayOfDrivers, string) {
+    const matchArray = [];
+
+    for (const driver of arrayOfDrivers) {
+        if (driver[0] === string[0]) {
+            matchArray.push(driver);
+        }
+    }
+    return matchArray;
+}
+
+function matchName (arrayOfDriversObject, string) {
+    const matchingDriver = [];
+
+    for (const driver of arrayOfDriversObject) {
+        if (driver.name ===  string) {
+            matchingDriver.push(driver)
+        }
+    }
+    return matchingDriver;
+}
